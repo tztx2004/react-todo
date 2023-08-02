c6487ff6d0f0f76dfc6f892c3fde37cf93fee286
tztx2004 <tztx2004@gmail.com>
2023-08-03 00:33:19 +0900

    개별 수정 기능 추가

--- src/components/List.js
+++ src/components/List.js
@@ -1,53 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 
-const List=React.memo(({
-  id,
-  title,
-  completed,
-  todoData,
-  setTodoData,
-  provided,
-  snapshot,
-  handleClick
-})=>{
-  
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-  return (
-    <div
-      key={id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${
-        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600`}
-    >
-      <div className="items-center">
-        <input
-          type="checkbox"
-          defaultChecked={completed}
-          onChange={() => handleCompleteChange(id)}
-        />{" "}
-        <span className={completed ? "line-through" : undefined}>{title}</span>
-      </div>
-      <div className="items-center">
-        <button
-          className="px-4 py-2 float-right"
-          onClick={() => handleClick(id)}
+const List = React.memo(
+  ({
+    id,
+    title,
+    completed,
+    todoData,
+    setTodoData,
+    provided,
+    snapshot,
+    handleClick,
+  }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
+    const handleCompleteChange = (id) => {
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.completed = !data.completed;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+    };
+
+    const handleEditChange = (event)=>{
+      setEditedTitle(event.target.value)
+    }
+
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+      
+      let newTodoData = todoData.map(data=>{
+        if(data.id === id){
+          data.title = editedTitle
+        }
+        return data
+      })
+      setTodoData(newTodoData)
+      setIsEditing(false)
+    }
+
+    if (isEditing) {
+      return (
+        <div
+          className={`flex items-center justify-between w-full px-4 py-1 my-2 bg-gray-100 text-gray-600`}
         >
-          X
-        </button>
-      </div>
-    </div>
-  );
-})
+          <div className="items-center">
+            <form onSubmit={handleSubmit}>
+              <input
+                value={editedTitle}  
+                onChange={handleEditChange}
+                className="w-full px-3 py-3 py-2 mr-4 text-gray-500 rounded"
+              />
+            </form>
+          </div>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => setIsEditing(false)}
+            >
+              X
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="px-4 py-2 float-right"
+              type="submit"
+            >
+              save
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          key={id}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          className={`${
+            snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+          } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600`}
+        >
+          <div className="items-center">
+            <input
+              type="checkbox"
+              defaultChecked={completed}
+              onChange={() => handleCompleteChange(id)}
+            />{" "}
+            <span className={completed ? "line-through" : undefined}>
+              {title}
+            </span>
+          </div>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => handleClick(id)}
+            >
+              X
+            </button>
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => setIsEditing(true)}
+            >
+              edit
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
+);
 
-export default List
+export default List;
