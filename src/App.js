1a749fc96a3c5755d271224111193e35dd3a4b84
tztx2004 <tztx2004@gmail.com>
2023-08-02 10:29:35 +0900

    tailwind 설치

--- src/App.js
+++ src/App.js
@@ -29,15 +29,13 @@ export default function App(){
     setValue("")
   };
 
-
-
   return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-
+          
           <List todoData={todoData} setTodoData={setTodoData} />
 
           <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
